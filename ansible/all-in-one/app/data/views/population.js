95c472e992d5b7b9780891e8b54b252df1910fb7
Starry326 <563978329@qq.com>
2020-05-11 18:37:04 +0800

    test

---
+++ ansible/all-in-one/app/data/views/population.js
@@ -0,0 +1,76 @@
+{
+    "views": {
+        "0-4-count": {
+            "map": "function(doc) {emit(doc.properties.lga_code, doc.properties._0_4_yrs_proj_count);}",
+            "reduce": "_sum"
+        },
+        "5-9-count": {
+            "map": "function(doc) {emit(doc.properties.lga_code, doc.properties._5_9_yrs_proj_count);}",
+            "reduce": "_sum"
+        },
+        "10-14-count": {
+            "map": "function(doc) {emit(doc.properties.lga_code, doc.properties._10_14_yrs_proj_count);}",
+            "reduce": "_sum"
+        },
+        "15-19-count": {
+            "map": "function(doc) {emit(doc.properties.lga_code, doc.properties._15_19_yrs_proj_count);}",
+            "reduce": "_sum"
+        },
+        "20-24-count": {
+            "map": "function(doc) {emit(doc.properties.lga_code, doc.properties._20_24_yrs_proj_count);}",
+            "reduce": "_sum"
+        },
+        "25-29-count": {
+            "map": "function(doc) {emit(doc.properties.lga_code, doc.properties._25_29_yrs_proj_count);}",
+            "reduce": "_sum"
+        },
+        "30-34-count": {
+            "map": "function(doc) {emit(doc.properties.lga_code, doc.properties._30_34_yrs_proj_count);}",
+            "reduce": "_sum"
+        },
+        "35-39-count": {
+            "map": "function(doc) {emit(doc.properties.lga_code, doc.properties._35_39_yrs_proj_count);}",
+            "reduce": "_sum"
+        },
+        "40-44-count": {
+            "map": "function(doc) {emit(doc.properties.lga_code, doc.properties._40_44_yrs_proj_count);}",
+            "reduce": "_sum"
+        },
+        "45-49-count": {
+            "map": "function(doc) {emit(doc.properties.lga_code, doc.properties._45_49_yrs_proj_count);}",
+            "reduce": "_sum"
+        },
+        "50-54-count": {
+            "map": "function(doc) {emit(doc.properties.lga_code, doc.properties._50_54_yrs_proj_count);}",
+            "reduce": "_sum"
+        },
+        "55-59-count": {
+            "map": "function(doc) {emit(doc.properties.lga_code, doc.properties._55_59_yrs_proj_count);}",
+            "reduce": "_sum"
+        },
+        "60-64-count": {
+            "map": "function(doc) {emit(doc.properties.lga_code, doc.properties._60_64_yrs_proj_count);}",
+            "reduce": "_sum"
+        },
+        "65-69-count": {
+            "map": "function(doc) {emit(doc.properties.lga_code, doc.properties._65_69_yrs_proj_count);}",
+            "reduce": "_sum"
+        },
+        "70-74-count": {
+            "map": "function(doc) {emit(doc.properties.lga_code, doc.properties._70_74_yrs_proj_count);}",
+            "reduce": "_sum"
+        },
+        "75-79-count": {
+            "map": "function(doc) {emit(doc.properties.lga_code, doc.properties._75_79_yrs_proj_count);}",
+            "reduce": "_sum"
+        },
+        "80-84-count": {
+            "map": "function(doc) {emit(doc.properties.lga_code, doc.properties._80_84_yrs_proj_count);}",
+            "reduce": "_sum"
+        },
+        "over85-count": {
+            "map": "function(doc) {emit(doc.properties.lga_code, doc.properties._85_yrs_over_proj_count);}",
+            "reduce": "_sum"
+        }
+    }
+}
---
+++ ansible/all-in-one/app/flaskPart.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, Response, render_template, make_response
+from flask_restful import Api, Resource, reqparse, abort
+from dataProcess import gainTweetsData, gainAurinData, gainProportion
+import os.path
+
+def create_app():
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    templates_dir = os.path.join(BASE_DIR, 'templates')
+    static_dir = os.path.join(BASE_DIR, 'static')
+    app = Flask(__name__, template_folder=templates_dir, static_folder=static_dir)
+    return (app)
+
+app = create_app()
+api = Api(app)
+
+
+class gainData(Resource):
+    def get(self, region):
+        tweetData = gainTweetsData(region)
+        aurinData = gainAurinData(region)
+        proportion = gainProportion(aurinData)
+        results = dict()
+        results['population_data'] = proportion
+        results['tweets_count'] = tweetData
+        return(results)
+
+api.add_resource(gainData, '/starry_app/gain_data/<region>')
+
+class Index(Resource):
+    def get(self):
+        return make_response(render_template('index.html', current_ip='starry'))
+
+api.add_resource(Index, '/index')
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",port=5000,debug=True)
